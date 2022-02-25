3e6a93bd1223b2d41525934cec4d173d8022ccc7
mjfox33 <mjfox33@gmail.com>
2022-02-25 11:02:51 -0500

    rewritten from recursion to dp

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -1,9 +1,11 @@
-import functools
 class Solution:
-    @functools.lru_cache(None)
     def climbStairs(self, n: int) -> int:
         if n < 0:
             return 0
         if n < 3:
             return n
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        previous = 2
+        previous_previous = 1
+        for _ in range(3,n+1):
+            previous, previous_previous = previous + previous_previous, previous
+        return previous
